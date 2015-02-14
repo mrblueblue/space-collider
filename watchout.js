eafa2a6f54dba157370d38f76aefd3971c33d0f0
Jonathan Huang <grokut@gmail.com>
2015-02-13 22:50:16 -0800

    added donut, aliens, and monolith

--- watchout.js
+++ watchout.js
@@ -11,40 +11,88 @@ var createEnemies = function(num) {
   return enemies;
 };
 
+var createAliens = function(num){
+  var aliens = [];
+
+  for (var i = 0; i <= num; i++){
+    aliens.push({
+      id:i,
+      x: Math.random() * 1000,
+      y: Math.random() * 750
+    });
+  }
+
+  return aliens;
+}
+
 var width = 1000,
   height = 750;
 
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
-  .style('background-color', 'black');
+  .style('background-image', 'url(space.gif)');
 
-var player = svg.selectAll("image").data([0]);
+var space = {
+  img: "spaceship.gif",
+  x: 500,
+  y: 300,
+  class: 'spaceship'
+};
+
+var donut = {
+  img: "donut.gif",
+  x: 400,
+  y: 300,
+  class: 'donut'
+};
+
+var monolith = {
+  img: "monolith.gif",
+  x: 800,
+  y: 500,
+  class: 'monolith'
+}
+
+var player = svg.selectAll("image").data([space,donut, monolith]);
 
 var spaceship = player.enter()
   .append("svg:image")
-  .attr("xlink:href", "spaceship.gif")
+  .attr("xlink:href", function(d){return d.img})
   .attr("width", "35")
   .attr("height", "35")
-  .attr("x", "500")
-  .attr("y", "300")
-  .classed('spaceship', true);
+  .attr("x", function(d){return d.x})
+  .attr("y", function(d){return d.y})
+  .attr('class', function(d){return d.class})
+
 
 var drag = d3.behavior.drag().on('drag', function(){
-  spaceship.attr('x', d3.event.x)
+  d3.select('.spaceship').attr('x', d3.event.x)
            .attr('y', d3.event.y);});
 
 d3.selectAll(".spaceship").call(drag);
 
-var enemies =  createEnemies(30);
+var enemies =  createEnemies(10);
+var aliens = createAliens(5);
 
-var img = svg.selectAll("image").data(enemies);
+var img = svg.selectAll("image").data(enemies)
+
+
+var imgAlien = svg.selectAll("image").data(aliens)
+  .enter()
+  .append("svg:image")
+  .attr("xlink:href",'alien.png')
+  .attr("width", '30')
+  .attr("height", '30')
+  .attr("x", function(d){return d.x})
+  .attr("y", function(d){return d.y})
+  .classed('aliens',true)
 
 var asteroids = img.enter()
   .append("svg:image")
   .attr("xlink:href",'asteroid.png')
-  .attr("width", '25')
-  .attr("height", '25')
+  .attr("width", '20')
+  .attr("height", '20')
   .attr("x", function(d){return d.x})
   .attr("y", function(d){return d.y});
 
@@ -88,9 +136,25 @@ var updatePosition = function(){
         checkCollision(enemy, onCollision);
       };
     });
+
+  imgAlien.transition().duration(1000)
+    .attr("x",function(d){return Math.random() * 1000})
+    .attr("y",function(d){return Math.random() * 1000})
 };
 
 setInterval(function(){
   updatePosition()}, 2000);
+
+setInterval(function(){
+  d3.select('.monolith')
+    .attr("x",function(d){return Math.random() * 2000})
+    .attr("y",function(d){return Math.random() * 2000})
+  }, 500);
+
+setInterval(function(){
+  d3.select('.donut').transition().duration(1000)
+    .attr("x",function(d){return Math.random() * 1000})
+    .attr("y",function(d){return Math.random() * 1000})
+  }, 500);
 
 
